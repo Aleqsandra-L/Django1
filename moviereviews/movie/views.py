9ba17c2f0e215d6bb419094d09a8b99b1695bde6
Aleqsandra-L <aleqsandra.lewandowska.s@gmail.com>
2024-06-07 09:57:45 +0200

    07.06 9.57

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h2>witam na stronie glownej</h2>')
+
+def about(request):
+    return HttpResponse('<h1>Strona o mnie</h1>')
