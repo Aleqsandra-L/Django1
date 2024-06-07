1a88083ae912c2fb06a2295c7b72c9e1ff292610
Aleqsandra-L <aleqsandra.lewandowska.s@gmail.com>
2024-06-07 13:01:02 +0200

    07.06 13.00

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Movie
+
+
 # Create your views here.
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return  render(request, 'home.html', {'name':'Ola',
-                                          'searchTerm': searchTerm})
+    movies = Movie.objects.all()
+    return  render(request, 'home.html', {'searchTerm': searchTerm,
+                                          'movies':movies})
 
 def about(request):
     return HttpResponse('<h1>Strona o mnie</h1>')
